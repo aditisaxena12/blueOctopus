7b137976768d296609d0d6c995b76395d9baf697
Aditi Saxena <aditi.saxena.18001@iitgoa.ac.in>
2024-02-25 14:03:26 +0530

    updated port

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var app = (0, express_1.default)();
-var port = 3001;
+var port = 8080;
 app.get('/', function (req, res) {
     res.send('Hello from the backend!');
 });
